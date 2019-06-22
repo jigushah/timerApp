85213a502646039917c0f8d54f843fa1c8f2309e
jigushah159 <jigushah159@gmail.com>
2019-06-22 18:21:44 +0530

    fix crash

--- app/commonComponent/popupComponent.js
+++ app/commonComponent/popupComponent.js
@@ -13,15 +13,18 @@ class PopupComponent extends React.Component {
   render() {
     let { event } = this.props;
     let check = '';
-    if(event.isActive === 1 && event.mid == 0 && !event.midAttachment){
-      check = 'Mid Check'
-    } else if(event.isActive === 2 && event.final == 0 && !event.finalAttachment){
-      check = 'Final Check'
+    if(event) {
+      if(event.isActive === 1 && event.mid == 0 && !event.midAttachment){
+        check = 'Mid Check'
+      } else if(event.isActive === 2 && event.final == 0 && !event.finalAttachment){
+        check = 'Final Check'
+      }
     }
+    
     return (<View style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0, padding: 20, flex: 1 }}>
       <View style={{ backgroundColor: 'white' }}>
         <Title text="Notification" customStyle={{ padding: 10, color: "black"}} />
-        <Title text={`Location: ${event.eventLocation}`} customStyle={{ padding: 10, color: "black" }} />
+        {event && <Title text={`Location: ${event.eventLocation}`} customStyle={{ padding: 10, color: "black" }} />}
         <Title text={`${check}, Confirm area checked?`} customStyle={{ padding: 10, color: "black" }} />
         <View style={{flexDirection:'row', justifyContent:'space-around', padding :10}}>
         <TouchableOpacity onPress={() => {
