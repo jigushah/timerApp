8bcf6d4fa26661abfcc407929b0d5bafbdf97b6f
jigushah159 <jigushah159@gmail.com>
2019-08-22 22:44:39 +0530

    fix timer on kill

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Text, StatusBar } from 'react-native';
 import { createBottomTabNavigator, createAppContainer, SafeAreaView } from 'react-navigation';
 import { Provider } from 'react-redux'
-import { store } from './app/store/store'
+import { store, persistor } from './app/store/store'
 import NavigatorService from './app/screens/navigator';
 import RootStack from './app/screens/tabs/tabs'
+import { PersistGate } from 'redux-persist/integration/react';
 
 let Navigation = createAppContainer(RootStack);
 
@@ -13,6 +14,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
+        <PersistGate persistor={persistor}>
         <SafeAreaView style={{flex : 1}}>
             <StatusBar backgroundColor="#ED1C24" barStyle="light-content" />
           <Navigation
@@ -20,6 +22,7 @@ export default class App extends React.Component {
                   NavigatorService.setContainer(navigatorRef);
               }}/>
         </SafeAreaView>
+        </PersistGate>
       </Provider>
     );
   }
